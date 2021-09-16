2a0e12749a6215e43a92f29b3983524f41f9585b
rzkamalia81 <90706601+rzkamalia81@users.noreply.github.com>
2021-09-16 16:26:39 +0700

    Update FCBN.cpp

--- FCBN.cpp
+++ FCBN.cpp
@@ -1,5 +1,7 @@
 %writefile FCBN.cpp
 
+#include<FCBN.h>
+
 FCBN::FCBN(int in_features, int out_features){
     ln = register_module("ln", torch::nn::Linear(in_features, out_features));
     bn = register_module("bn", torch::nn::BatchNorm1d(out_features));
