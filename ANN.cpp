1d8e2eaffc9268888425cd0e46ffed5e555db4e2
rzkamalia81 <90706601+rzkamalia81@users.noreply.github.com>
2021-09-16 16:23:36 +0700

    Create ANN.cpp

---
+++ ANN.cpp
@@ -0,0 +1,18 @@
+%%writefile ANN.cpp
+
+ANN::ANN(){
+    input = FCBN(node[0], nodes[1]);
+    hidden = FCBN(nodes[1], nodes[2]);
+    output = FCBN(nodes[2], nodes[3]);
+
+    input = register_module("input", input);
+    hidden = register_module("hidden", hidden);
+    output = register_module("output", output);
+    }
+
+torch::Tensor ANN::forward(torch::Tensor x){
+    x = input->forward(x);
+    x = hidden->forward(x);
+    x = output->forward(x);
+    return x;
+    }
